91bef3aa370195526b52281a9da98ddf799ead85
Selin Karabıçak <selinss62@gmail.com>
2024-08-07 10:45:50 +0300

    Authentication done

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,24 @@
+import type {Session, User} from 'next-auth'
+import type {JWT} from 'next-auth/jwt'
+
+
+type userId = string 
+
+
+declare module 'next-auth/jwt' {
+    interface JWT {
+        user: {
+            id: userId
+        }
+    }
+}
+
+
+declare module 'next-auth' {
+    interface Session {
+        user: User & {
+            id: UserId
+        }
+            
+  }
+}
